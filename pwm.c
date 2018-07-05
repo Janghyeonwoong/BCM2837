7628c12014f585798ceec57032ab74e7f666ed3c
Janghyeonwoong <38995335+Janghyeonwoong@users.noreply.github.com>
2018-07-05 16:39:46 +0900

    Add files via upload

--- pwm.c
+++ pwm.c
@@ -16,12 +16,14 @@
 #define GPFSEL1 0x4
 #define GPFSEL2 0x8
 #define GPFSEL3 0xC
+#define GPLEV 0x34
 #define GPEDS 0x40
-#define GPAFEN 0x88
-#define GPAREN 0x7C
+#define GPFEN 0x58
+#define GPREN 0x4C
 #define GPSET 0x1C
 #define GPLEV 0x34
 #define GPPUD 0x94
+#define GPAFEN 0x88
 
 #define CTL 0x0
 #define STA 0x4
@@ -31,12 +33,31 @@
 #define CLOCK_CNTL 0xA0
 #define CLOCK_DIV 0xA4
 
+typedef struct GPSEL {
+    volatile unsigned int sel0:3;
+    volatile unsigned int sel1:3;
+    volatile unsigned int sel2:3;
+    volatile unsigned int sel3:3;
+    volatile unsigned int sel4:3;
+    volatile unsigned int sel5:3;
+    volatile unsigned int sel6:3;
+    volatile unsigned int sel7:3;
+    volatile unsigned int sel8:3;
+    volatile unsigned int sel9:3;
+    volatile unsigned int reserve:2;
+
+} GPSEL;
+
 
 volatile unsigned int * init_gpio(int fd);
 volatile unsigned int * init_pwm(int fd);
 volatile unsigned int * init_clk(int fd);
 void change_pwm(volatile unsigned int * pwm, int percent);
 void check_falling_edge(volatile unsigned int * gpio, int num);
+int getbit(volatile unsigned int x, int n);
+void setbit(volatile unsigned int * x, int n);
+
+
 
 int main(void)
 {
@@ -44,38 +65,37 @@ int main(void)
     int per;
     volatile unsigned int * gpio = init_gpio(fd);    
     volatile unsigned int * pwm = init_pwm(fd);
-	volatile unsigned int * clk = init_clk(fd);	
+    volatile unsigned int * clk = init_clk(fd);	
 	if(close(fd) < 0){ //No need to keep mem_fd open after mmap
                          //i.e. we can close /dev/mem
 	perror("couldn't close /dev/mem file descriptor");
     exit(1);
 	}	
+    GPSEL* SEL1 = &gpio[GPFSEL1/4];
 // we will use gpio 18 for PWM
 // and pwm period is 1kHz  
-	gpio[GPFSEL1/4] &= ~(7<<24);
-	gpio[GPFSEL1/4] |= (2<<24);
+	SEL1->sel8 = 2;
 	clk [CLOCK_CNTL/4] = 0x5A000000 | (1 << 5);
 	usleep(10);
 	while( clk[CLOCK_CNTL/4] & 0x00000080){}
 	clk[CLOCK_DIV/4] = 0x5A000000 | (75 << 12); // 19.2Mhz / 75
 	clk[CLOCK_CNTL/4] = 0x5A000011;
-	gpio[GPFSEL2/4] &= ~(7<<9);	
-	gpio[GPFSEL2/4] |= (0<<9);
-	gpio[GPAFEN/4] |= (1<<23);
+	SEL1 -> sel7 = 0;
+	setbit(&gpio[GPFEN/4] , 17);
 	gpio[GPPUD/4] = 1;
+	setbit(&gpio[GPEDS/4] , 17);
 
 	pwm[CTL / 4] = 0;
 	usleep(10);
 	pwm[RNG1/4] = 256;
 	usleep(10);
-	change_pwm(pwm , 50);
+	change_pwm(pwm , 90);
 	
 	pwm[CTL/4] |= (1 << 0);	
 
 	while(1)
 	{
-	scanf("%d", &per);
-	change_pwm(pwm, per);
+	check_falling_edge(gpio,17);
 		
 	}
 
@@ -154,7 +174,18 @@ void change_pwm(volatile unsigned int * pwm , int percent)
 }
 void check_falling_edge(volatile unsigned int * gpio, int num)
 {
-	if(gpio[GPAFEN] & 0x00800000)
-			printf("Falling edge detected..\n");
-
+//	printf("LEV \ %d \n", getbit(gpio[GPLEV/4],17) );
+	if(getbit(gpio[GPEDS/4], 17) == 1)
+	{
+	printf("Falling edge detected..\n");
+	}
+	
+}
+int getbit(volatile unsigned int x, int n)
+{
+    return (x &(1<<n))>>n;
+}
+void setbit(volatile unsigned int * x, int n)
+{
+    (*x) |= (1 << n);
 }
