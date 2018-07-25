c8aa61549d1dbafe34a36442979691a03a35e99e
Hywon woong Jang <jhw3169@naver.com>
2018-07-25 11:43:36 +0900

    First edition Ver 1.1  catch grammar error

--- main.cpp
+++ main.cpp
@@ -68,82 +68,40 @@ int main(void)
 	return 0;
 }
 
-class image_and_capture{
-	private:
-		Mat image; // capture image
-		Mat roi; // set the roi for counting circles
-		vector <Vec3f> circles; // for using HoughCircles
-		Mat images[3]; // for using histogram and similarlity
-		vector <Mat> histograms;
-		Mat gray;
-		double per;
-		double similar;
-	public:
-		VideoCapture capture; // usb video camera
-		image_and_capture() : image(), roi() ,caputre(0), per(0), similar(0)
+int image_and_capture::capture_3min(void)
+{
+	while (stop == false)
+	{
+		capture >> image;
+		per = percent(image);
+		if (per > double(seg_value * 10))
 		{
-			circles.reserve(3);
-			histograms.reserve(4);
+			if (capture_30sec())
+				return MOTOR;
 		}
-		image_and_capture(char * name) : roi(), capture(0), per(0), similar(0)
-		{
-			imread(name, image);
-			circles.reserve(3);
-			histograms.reserve(4);
-		}
-		image_and_capture(char * name, int num) : roi(), capture(num), per(0), similar(0) 
-		{
-			imread(name, image);
-			circles.reserve(3);			
-			histograms.reserve(4);
-		}
-		int capture_3min(void); // 
-		bool capture_30sec(void);
-		void cutting_image(Mat image, Rect(0, 480 - 55, 120, 54) );
-		double percent(Mat image); // change image to HSV and extract yellow color
-		void calc_histogram(Mat image); // make two dimension histogram H(hue) S(saturation
-		double check_similarity(Mat source, Mat compare); // find the value that similarity
-		int counting_circle(Mat image); // find circles at roi 
-		void video_capture(void);
+		sleep(60 * 3);
+	}
+	return STOP;
+
 }
-/* * you have to make enum fucntion for switch-case 
-   *
-   */ 
-
-int image_and_cpature::capture_3min(void)
-	{
-		while(stop == false)
-		{
-			vc >> image;
-			per = percent(image);
-			if(per > double(seg_value * 10) )
-			{
-				if(capture_30sec())
-					return MOTOR;
-			}
-			sleep(60*3);
-		}
-		return STOP;
-
-	}
 bool image_and_capture::capture_30sec(void)
+{
+	histograms.clear();
+	calc_histogram(image);
+	for (int i = 0; i<3; i++)
 	{
-		histograms.clear();
-		calc_histogram(image);
-		for(int i=0; i<3 ; i++)
+		sleep(30);
+		capture >> images[i];
+		calc_histogram(images[i]);
+		similar = check_similarity(histograms[0], histograms[i + 1]);
+		if (similar < 90)
 		{
-			sleep(30);
-			vc >> images[i];
-			calc_histogram(images[i]);
-			similar = check_similarity(histograms[0] , histograms[i+1]);
-			if(similar < 90)
-			{
-				cout << "similarity < 90" << endl;
-				return false;
-			}
+			cout << "similarity < 90" << endl;
+			return false;
 		}
-		return true;
 	}
+	return true;
+}
 void image_and_capture::cutting_image(Mat image, Rect rec = Rect(0, 480 - 55, 120, 54))
 {
 	roi = image(rec);
@@ -175,19 +133,20 @@ void image_and_capture::calc_histogram(Mat image)
 	static Vec3i bins(30, 42, 0);
 	static Vec3f range(180, 256, 0);
 
-	cvtColor(image, Hsv, CV_BGR2HSV);
+	cvtColor(image, hsv, CV_BGR2HSV);
 
 	static int channels[] = { 0 ,1 };
-	static int histSize[] = { bins[0], bins[1]};
+	static int histSize[] = { bins[0], bins[1] };
 
 	static float range1[] = { 0 , range[0] };
 	static float range2[] = { 0 , range[1] };
-	
-	const float*  ranges[] = { range1, range2};
 
-	calcHist(hsv, 1, channels, Mat(), hist, 2, histSize, ranges);
+	const float*  ranges[] = { range1, range2 };
+
+	calcHist(&hsv, 1, channels, Mat(), hist, 2, histSize, ranges);
+	
 	normalize(hist, hist, 0, 1, NORM_MINMAX);
-	(this->histograms).pushback(hist);
+	histograms.push_back(hist);
 }
 
 double image_and_capture::check_similarity(Mat source, Mat compare)
@@ -198,22 +157,22 @@ double image_and_capture::check_similarity(Mat source, Mat compare)
 
 int image_and_capture::counting_circle(Mat image)
 {
-	color(image, image, CV_BGR2GRAY);
+	cvtColor(image, image, CV_BGR2GRAY);
 	//GaussianBlur(gray, gray, Size(9, 9), 2, 2);
 	HoughCircles(image, this->circles, CV_HOUGH_GRADIENT, 1, 100, 100, 40, 0, 100);
-	return (thie->circles).size();
+	return (this->circles).size();
 }
 
 void image_and_capture::video_capture(void)
 {
 	int i(0);
 	circles.clear();
-	while(i == 0)
+	while (i == 0)
 	{
 		capture >> image;
 		cutting_image(image);
 		i = counting_circle(roi);
 	}
-	
+
 
 }
